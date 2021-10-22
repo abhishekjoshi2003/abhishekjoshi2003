9e54824ed4fd140158af163083a4647558013347
abhishekjoshi2003 <91530015+abhishekjoshi2003@users.noreply.github.com>
2021-10-22 09:02:44 -0700

    Add files via upload
    This is a pattern of right angle hollow triangle in right hand side of n row and columns . Here n is user input to code .

---
+++ Pattern10.java
@@ -0,0 +1,34 @@
+import java.util.Scanner		;
+public class Pattern10
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in)	;
+		int n  = sc.nextInt()			;
+		for(int i=1;i<=n-1;i++)		
+		{
+			for(int j=1;j<=1;j++)
+			{
+				System.out.print(" * ")	;
+			}
+			for(int j=1;j<=i-2;j++)
+			{
+				System.out.print("   ")	;
+			}
+			if(i!=1)
+			{
+			for(int j=2;j<=2;j++)
+			{
+				System.out.print(" * ")	;
+			}
+			}
+			
+			System.out.println()		; 
+		}
+		for(int j=1;j<=n;j++)
+		{
+			System.out.print(" * ")		;
+		}
+	}
+}
+		
