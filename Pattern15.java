ab96c6f0d7835bff963a894c2c00c75f81003a7d
abhishekjoshi2003 <91530015+abhishekjoshi2003@users.noreply.github.com>
2021-10-24 08:25:19 -0700

    Add files via upload
    This is a pattern of frame in which a kite is made with spaces of n rows and columns . Here n is user input  .

---
+++ Pattern15.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Pattern15
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in)		;
+		int n = sc.nextInt()				;
+		int sp = 1					;
+		for(int i = 1; i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" * ")		;
+			}
+			for(int j=1;j<=sp;j++)
+			{
+				System.out.print("   ")		;
+			}
+			sp = sp + 2				;
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" * ")		;
+			}
+			System.out.println()			;
+		}
+		sp = sp - 4					;
+		for(int i=1;i<=n-1;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" * ")		;
+			}
+			for(int j=1;j<=sp;j++)
+			{
+				System.out.print("   ")		;	
+			}
+			sp = sp -2				;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" * ")		;
+			}
+			System.out.println()			;
+        	}
+	}
+} 
